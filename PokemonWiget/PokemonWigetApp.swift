bcbed90ce3227afa2d32480c18c0efb6f43b04a6
Etsushi <324etsushi@gmail.com>
2023-01-29 09:42:14 +0900

    Initial Commit

---
+++ PokemonWiget/PokemonWigetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonWigetApp.swift
+//  PokemonWiget
+//
+//  Created by Etsushi Otani on 2023/01/29.
+//
+
+import SwiftUI
+
+@main
+struct PokemonWigetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
