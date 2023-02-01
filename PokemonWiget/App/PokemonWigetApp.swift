8df1822885842fffd8335bc0b9cd9ddd066f8361
Etsushi <324etsushi@gmail.com>
2023-02-01 17:26:15 +0900

    Appディレクトリ作成

--- PokemonWiget/App/PokemonWigetApp.swift
+++ PokemonWiget/App/PokemonWigetApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PokemonWigetApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(pokeApiProvider: PokeAPIProviderImpl())
         }
     }
 }
